97b73c9f743e75e957ba550871efbdbc218633ec
Ohad Ben-Cohen <ohad@wizery.com>
2013-11-10 20:55:18 +0200

    tmp hack: poll wlan irq

--- drivers/mmc/core/sdio_irq.c
+++ drivers/mmc/core/sdio_irq.c
@@ -110,8 +110,8 @@ static int sdio_irq_thread(void *_host)
 	 * hence we poll for them in that case.
 	 */
 	idle_period = msecs_to_jiffies(10);
-	period = (host->caps & MMC_CAP_SDIO_IRQ) ?
-		MAX_SCHEDULE_TIMEOUT : idle_period;
+//	period = (host->caps & MMC_CAP_SDIO_IRQ) ?
+//		MAX_SCHEDULE_TIMEOUT : idle_period;
 
 	pr_debug("%s: IRQ thread started (poll period = %lu jiffies)\n",
 		 mmc_hostname(host), period);
